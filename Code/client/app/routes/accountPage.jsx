a8d80beff8b4995f66733b1c879a9d44b803db96
Esther van Driessche <esther.van.driessche@student.howest.be>
2025-05-29 09:18:16 +0200

    #1 added supabase and account with magic link

---
+++ Code/client/app/routes/accountPage.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+import { supabase } from '../supabaseClient'
+import Auth from '../components/Auth'
+import Account from '../components/Reg'
+
+const AccountPage = () => {
+    const [session, setSession] = useState(null)
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session)
+        })
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        })
+    }, [])
+    return (
+        <div className="container" style={{ padding: '50px 0 100px 0' }}>
+            {!session ? <Auth /> : <Account key={session.user.id} session={session} />}
+        </div>
+    )
+};
+
+export default AccountPage;
