59e87fba0ddb1025fa9a7b4027ade0609ba02d04
Esther van Driessche <esther.van.driessche@student.howest.be>
2025-05-29 09:18:16 +0200

    #1 added supabase and account with magic link

--- Code/client/app/routes.js
+++ Code/client/app/routes.js
@@ -1,7 +1,8 @@
-import { route, layout, index } from "@react-router/dev/routes";
+import { route, index } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.jsx"),
   route("/campaign", "routes/homeCampaign.jsx"),
-  route("/arduino", "routes/homeArduino.jsx")
+  route("/arduino", "routes/homeArduino.jsx"),
+  route("/account", "routes/accountPage.jsx"),
 ];
