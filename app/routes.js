3214e84cf78bdb873738dc3d5bb3a4ab177725fe
Esther van Driessche <esther.van.driessche@student.howest.be>
2025-05-30 11:32:00 +0200

    removed arduino route

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,5 @@ import { route, index } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.jsx"),
-  route("/arduino", "routes/homeArduino.jsx"),
   route("/account", "routes/accountPage.jsx"),
 ];
