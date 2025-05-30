98fd036a8ec5b0af676ecca5430c75b1ec3b6f0c
Esther van Driessche <esther.van.driessche@student.howest.be>
2025-05-30 13:47:47 +0200

    removed unecessary files

--- Code/client/app/routes.js
+++ Code/client/app/routes.js
@@ -2,7 +2,5 @@ import { route, index } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.jsx"),
-  route("/campaign", "routes/homeCampaign.jsx"),
-  route("/arduino", "routes/homeArduino.jsx"),
   route("/account", "routes/accountPage.jsx"),
 ];
