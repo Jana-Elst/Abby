232184d2e8d1c592ec6a61ad516040b2974d9341
Jana <jana.elst@student.howest.be>
2025-06-05 14:28:22 +0200

    date, hour, title --> added to detailpage

--- app/routes/detailAbbymoments.jsx
+++ app/routes/detailAbbymoments.jsx
@@ -1,13 +1,64 @@
 import { useMatch } from "react-router-dom";
 
-const DetailClock = () => {
-    const match = useMatch("/:firstRoute/:secondRoute/*");
-    const id = match.params["*"];
+import Title from "../components/molecules/title";
+import Button from "../components/molecules/button"
+
+import { getTime } from "../services/clock";
+
+//get clock
+import { getClock } from "../services/data";
+export async function clientLoader() {
+    //get clock id --> last element from querystring
+    let url = window.location.href;
+    url = url.split('/');
+    const id = url[url.length - 1];
+
+    //get data clock
+    let clock = await getClock(id);
+    clock = clock[0];
+    console.log(clock);
+    return { clock };
+}
+
+
+const DetailClock = ({ loaderData }) => {
+    const { clock } = loaderData;
 
     return (
         <>
             {/* //back button */}
-            <p>Dit is de detailpage van klok {`${id}`}</p>
+            <Title title={clock.name} />
+            <p>
+                {
+                    clock.startTime
+                        ? getTime(clock.startTime).date
+                        : getTime(clock.sheduledStartTime).date
+                }
+            </p>
+            <p>
+                {
+                    clock.startTime
+                        ? getTime(clock.startTime).time
+                        : getTime(clock.sheduledStartTime).time
+                }
+            </p>
+            <p>Klok</p>
+            <p>
+                {
+                    clock.description
+                        ? clock.description
+                        : ""
+                }
+            </p>
+            <p>deelnemers</p>
+            <button>
+                {
+                    clock.private
+                        ? "Je kan niet meedoen aan dit Abbymoment"
+                        : "Doe mee met dit Abbymoment"
+
+                }
+            </button>
         </>
     )
 };
--- app/services/clock.js
+++ app/services/clock.js
@@ -1,3 +1,5 @@
+//calculate the angle of the pointer of the clock
+//12 hours clock
 export const getAngle = (time) => {
     let [hours, minutes, seconds] = time.split(':').map(Number);
 
@@ -7,4 +9,23 @@ export const getAngle = (time) => {
     const angle = (Math.PI / 180) * totalDegrees;
 
     return (angle);
+}
+
+//rewrite the time
+export const getTime = (dateTime) => {
+    console.log(dateTime);
+    const [date, time] = dateTime.split("T");
+    const [hour, minutes, secondes] = time.split(":");
+    return (
+        {
+            date: getDate(date),
+            time: `${hour}:${minutes}`
+        }
+    )
+}
+
+export const getDate = (date) => {
+    const monthNames = ["januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december"];
+    const [year, month, day] = date.split("-");
+    return (`${day} ${monthNames[month-1]}`)
 }
