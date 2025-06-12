7179571366ad46089328444a0d6b87b57bb7cfc7
henrifoucher <henri.foucher@student.howest.be>
2025-06-12 10:48:04 +0200

    Changes Home page

--- app/components/frames/hero.jsx
+++ app/components/frames/hero.jsx
@@ -1,18 +1,21 @@
 import Title from "../molecules/title";
 import Button from "../molecules/button";
 
+import homeHero from "../../src/assets/home-hero.png";
+
 const Hero = () => {
     return (
         <div className="hero">
-            <Title>
-                Rush jij ook door het leven
+            <Title className="hero__title">
+                Rush jij ook door het <span className="hero__title-span">leven</span>
             </Title>
-            <p>Tijd om het wat rustiger aan te doen.</p>
-            <Button link={'#Abby'} icon={'skip'}>
+            <p className="hero__header">Tijd om het wat rustiger aan te doen.</p>
+            {/* <Button link={'#Abby'} icon={'skip'}>
                 Sla over
-            </Button>
+            </Button> */}
             {/* clock COMP */}
-            <Button link={'#Intro'}>
+            <img className="links__li__img" src={homeHero} alt="De Living van Abby" />
+            <Button link={'#Intro'} >
                 Waarom vertragen?
             </Button>
         </div >
