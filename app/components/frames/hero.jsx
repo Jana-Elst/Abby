1ca1c6e39ffc440f1276662341b3e1ff67822419
henrifoucher <henri.foucher@student.howest.be>
2025-06-12 11:13:42 +0200

    Changes root

--- app/components/frames/hero.jsx
+++ app/components/frames/hero.jsx
@@ -7,15 +7,15 @@ const Hero = () => {
     return (
         <div className="hero">
             <Title className="hero__title">
-                Rush jij ook door het <span className="hero__title-span">leven</span>
+                Rush jij ook door het <span className="green__fg">leven</span>
             </Title>
             <p className="hero__header">Tijd om het wat rustiger aan te doen.</p>
             {/* <Button link={'#Abby'} icon={'skip'}>
                 Sla over
             </Button> */}
             {/* clock COMP */}
-            <img className="links__li__img" src={homeHero} alt="De Living van Abby" />
-            <Button link={'#Intro'} >
+            <img className="hero__img" src={homeHero} alt="De Living van Abby" />
+            <Button link={'#Intro'} extraClass="btn__text hero__btn">
                 Waarom vertragen?
             </Button>
         </div >
