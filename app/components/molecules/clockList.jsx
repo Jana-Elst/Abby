9a9138e63321ee876ced611b28cea8d014957f42
Jana <jana.elst@student.howest.be>
2025-06-15 17:07:11 +0200

    their is something wrong with clockList

--- app/components/molecules/clockList.jsx
+++ app/components/molecules/clockList.jsx
@@ -14,20 +14,20 @@ const ClockList = ({ clocks = [], clockProfile, state }) => {
     const { userId } = useContext(UserContext);
 
     //My clocks
-    if (state === 'Mijn Abbymomenten') {
-        const myClockIds = clockProfile
-            .filter(cp => cp.profile_id === userId)
-            .map(cp => cp.clock_id);
-        const myClocks = clocks.filter(clock => myClockIds.includes(clock.id))
+    // if (state === 'Mijn Abbymomenten') {
+    //     const myClockIds = clockProfile
+    //         .filter(cp => cp.profile_id === userId)
+    //         .map(cp => cp.clock_id);
+    //     const myClocks = clocks.filter(clock => myClockIds.includes(clock.id))
 
-        console.log(state)
+    //     console.log(state)
 
         return (
             <div>
                 {
                     clocks.length ? (
                         <ul>
-                            {myClocks.map((clock) => (
+                            {clocks.map((clock) => (
                                 <ClockCard key={clock.id} clock={clock} clockProfile={clockProfile} state={state} />
                             ))}
                         </ul>
@@ -38,6 +38,6 @@ const ClockList = ({ clocks = [], clockProfile, state }) => {
             </div>
         )
     }
-}
+
 
 export default ClockList;
