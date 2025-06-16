13862c489c07bfecf600df98ab83938b70ac1b9e
henrifoucher <henri.foucher@student.howest.be>
2025-06-16 18:02:03 +0200

    Popup 80% fix

--- app/components/molecules/popUp.jsx
+++ app/components/molecules/popUp.jsx
@@ -1,8 +1,11 @@
 import './popUp.css'
 
+import iconWarning from "../../src/assets/icon-warning.svg"
+import iconClose from "../../src/assets/icon-close.svg"
+
 import Button from "./button";
 
-const PopUp = ({ children, setUiState, uiState }) => {
+const PopUp = ({ children, setUiState, uiState, extraClass }) => {
     const handleClickClose = () => {
         setUiState({
             ...uiState,
@@ -12,8 +15,11 @@ const PopUp = ({ children, setUiState, uiState }) => {
     }
 
     return (
-        <div className={uiState.popUpOpen ? 'open' : 'close'}>
-            <Button onClick={handleClickClose}>CLOSE X</Button>
+        <div className={`${uiState.popUpOpen ? 'open' : 'close'} ${extraClass}`}>
+            <div className='popup__header'>
+                <img className="popup__warning" src={iconWarning} alt="melding icon" />
+                <Button onClick={handleClickClose} extraClass="popup__btn"><img src={iconClose} alt="close icon" /></Button>
+            </div>
             {children}
         </div>
     )
