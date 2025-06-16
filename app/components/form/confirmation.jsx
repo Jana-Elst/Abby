bc10a5682ecffa958466e5a91a1ea8f1b0102ef6
Jana <jana.elst@student.howest.be>
2025-06-16 17:25:03 +0200

    Update confirmation.jsx

--- app/components/form/confirmation.jsx
+++ app/components/form/confirmation.jsx
@@ -8,7 +8,6 @@ import { useContext } from "react";
 
 //root variables
 import { UserContext } from '../../context/UserContext';
-import { Navigate } from "react-router";
 
 const Confirmation = ({ formState, setFormState, setFlowForm }) => {
     const { userId } = useContext(UserContext);
@@ -36,21 +35,19 @@ const Confirmation = ({ formState, setFormState, setFlowForm }) => {
 
         setFlowForm('plan');
     }
-    console.log(formState);
-{
-        return (
-            <div className="confirm">
-                {
-                    formState === 'now'
-                        ? <Title title={"Je moment is gestart!"} />
-                        : <Title title={"Je moment is gepland!"} />
-                }
-                <p className="confirm__title">Je moment is succesvol aangemaakt</p>
-                <Button onClick={removeStates} extraClass="btn__text confirm__btn purple__bg btn__arrow" link={`abbymomenten/${clockId}`}>Ga naar je moment<img className='btn__icon' src={arrow} alt="een pijl" /></Button>
-            </div>
 
-        )
-    }
+    return (
+        <div className="confirm">
+            {
+                formState === 'now'
+                    ? <Title title={"Je moment is gestart!"} />
+                    : <Title title={"Je moment is gepland!"} />
+            }
+            <p className="confirm__title">Je moment is succesvol aangemaakt</p>
+            <Button onClick={removeStates} extraClass="btn__text confirm__btn purple__bg btn__arrow" link={`abbymomenten/${clockId}`}>Ga naar je moment<img className='btn__icon' src={arrow} alt="een pijl" /></Button>
+        </div>
+
+    )
 };
 
 export default Confirmation;
