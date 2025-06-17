37f2f77075713735732bc57c8b8e57b582203eb4
Jana <jana.elst@student.howest.be>
2025-06-17 16:22:32 +0200

    Removed un used files and add readme

--- clocks/app/routes.js
+++
@@ -1,9 +0,0 @@
-import { index, route } from "@react-router/dev/routes";
-
-export default [
-  index("routes/home.jsx"),
-  route("/login", "routes/login.jsx"),
-  route("/form", "routes/form.jsx"),
-  route("/end", "routes/end.jsx"),
-  route("/test", "routes/arduinoTest.jsx"),
-];
